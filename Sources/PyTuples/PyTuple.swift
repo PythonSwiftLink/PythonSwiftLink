d743e682ac9414aeb1f7ed4bc3145bb2f8db7ae3
PythonSwiftLink <pythonswiftlink@gmail.com>
2025-01-14 07:25:43 +0100

    Update PyTuple.swift

--- Sources/PyTuples/PyTuple.swift
+++ Sources/PyTuples/PyTuple.swift
@@ -8,7 +8,7 @@ import PyDecode
 
 @inlinable public func PyTuple_GetItem<R: ConvertibleFromPython>(_ object: PyPointer?,_ index: Int) throws -> R {
 	guard let ptr = PyTuple_GetItem(object, index) else { throw PythonError.attribute }
-	defer { Py_DecRef(ptr) }
+    //defer { Py_DecRef(ptr) }
 	return try R(object: ptr)
 }
 
